2b52e08aae474c0dd66e9dc60d34cd067684c3ab
tceyhan <55tc155@gmail.com>
2023-07-02 18:13:28 +0300

    ModalForm added

--- app/components/studentList/StudenList.jsx
+++ app/components/studentList/StudenList.jsx
@@ -3,13 +3,18 @@ import React, { useEffect, useState } from "react";
 import UserCard from "../userCard/UserCard";
 import styles from "./styles.module.css";
 import { SearchIcon } from "../../../public/icons";
+import ModalForm from "../toggleModal/ModalForm";
+import { Button, Checkbox, Label, Modal, TextInput } from "flowbite-react";
 
 const StudenList = () => {
   const [student, setStudent] = useState([]);
+  const [openModal, setOpenModal] = useState();
+  const [modal, setModal] = useState(false);
+  
 
   const getUsers = async () => {
-    let students = await fetch("https://dummyjson.com/users").then((res) =>
-      res.json()
+    let students = await fetch("https://dummyjson.com/users/?limit=6").then(
+      (res) => res.json()
     );
     console.log(students.users);
     setStudent(students.users);
@@ -31,8 +36,17 @@ const StudenList = () => {
             placeholder="Search.."
           />
           <SearchIcon className={styles.searchIcon} />
-          <button className={styles.btn}>ADD NEW ACCOUNT</button>
+          <button
+            className={styles.btn}
+            onClick={() => {
+              setOpenModal("form-elements")
+              setModal(true)
+            }}
+          >
+            NEW ACCOUNT
+          </button>
         </div>
+       
       </div>
       <div className="flex flex-col justify-between">
         <ul className="flex justify-between items-center p-[30px]">
@@ -42,11 +56,14 @@ const StudenList = () => {
           <li className={styles.fieldText}>Phone</li>
           <li className={styles.fieldText}>Website</li>
           <li className={styles.fieldText}>Company Name</li>
-          <li className={styles.fieldText}></li>          
+          <li className={styles.fieldText}></li>
         </ul>
         {student?.map((user) => (
           <UserCard user={user} key={user.id} />
         ))}
+      </div>
+      <div>
+        {modal && <ModalForm openModal={openModal} setOpenModal={setOpenModal}/>}        
       </div>
     </div>
   );
